53e31ec1c3cd4b52c8e96914781f3e96555bd654
ctsygiel <ctsygiel@gmail.com>
2017-03-13 16:33:55 -1000

    passed dog object literal

--- basics.js
+++ basics.js
@@ -35,3 +35,10 @@ if(person.age >= 16){
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+ var dog = {
+    bark: function(){console.log("bark!");},
+    name: "Spot"
+ };
+
+
